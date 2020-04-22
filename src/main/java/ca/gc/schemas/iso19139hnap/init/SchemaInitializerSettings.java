3f610a94244796074194d544b630616a510dac8d
david blasby <david.blasby@geocat.net>
2020-04-22 11:45:27 -0700

    adding setting for controlling org name GUI

---
+++ src/main/java/ca/gc/schemas/iso19139hnap/init/SchemaInitializerSettings.java
@@ -0,0 +1,50 @@
+package ca.gc.schemas.iso19139hnap.init;
+
+import ca.gc.schema.iso19139hnap.util.XslUtilHnap;
+import org.fao.geonet.domain.Setting;
+import org.fao.geonet.domain.SettingDataType;
+import org.fao.geonet.events.server.ServerStartup;
+import org.fao.geonet.kernel.GeonetworkDataDirectory;
+import org.fao.geonet.repository.SettingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ * Creates Settings in the Settings table
+ * With a default value
+ */
+public class SchemaInitializerSettings implements
+    ApplicationListener<ServerStartup> {
+    @Autowired
+    private SettingRepository _settingRepository;
+
+    @PostConstruct
+    public void init() {
+    }
+
+    private void addSetting(String key,SettingDataType type, String value, int position ) {
+        if (_settingRepository.findOne(key) == null) {
+            Setting setting = new Setting();
+            setting.setDataType(type);
+            setting.setName(key);
+            setting.setPosition(position);
+            setting.setValue(value);
+            _settingRepository.save(setting);
+        }
+    }
+
+    private void addSettings() {
+        addSetting("schema/"+ XslUtilHnap.HNAP_SCHEMA_NAME+"/UseGovernmentOfCanadaOrganisationName",
+            SettingDataType.BOOLEAN,"true",1);
+    }
+
+    //when the server starts up, we are ready to
+    @Override
+    public void onApplicationEvent(ServerStartup event) {
+        addSettings();
+    }
+}
