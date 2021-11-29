7f501fd9d32682362073f416d816fbc354c06b4c
Jose García <josegar74@gmail.com>
2021-11-29 16:04:25 +0100

    Version 3.10.9

--- docs/fr/_static/documentation_options.js
+++ docs/fr/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.10-SNAPSHOT',
+    VERSION: '3.10.9',
     LANGUAGE: 'fr',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
